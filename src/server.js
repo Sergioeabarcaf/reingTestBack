f67da8c2637bed2260240d935645c2118d590d1f
sergioeabarcaf <sergioaf1991@gmail.com>
2020-02-14 21:56:06 -0300

    fix: use cors, allow connection from front

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import helmet from 'helmet';
+
 
 import routes from './routes';
 
@@ -8,6 +10,7 @@ const server = express();
 
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
+server.use(cors());
 server.use(helmet());
 server.use('/', routes);
 
